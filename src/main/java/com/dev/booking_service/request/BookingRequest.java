115590da190328b2d6e101bb3c657c1325cb02f9
Tassochaves <tassogt@hotmail.com>
2025-05-14 16:37:32 -0300

    Feat: adds BookingRequest attributes

--- src/main/java/com/dev/booking_service/request/BookingRequest.java
+++ src/main/java/com/dev/booking_service/request/BookingRequest.java
@@ -10,5 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookingRequest {
-    private String event;
+    private Long userId;
+    private Long eventId;
+    private Long ticketCount;
 }
